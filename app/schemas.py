a6ff0ec7cf1e3a34522d84eb6f561f02199b8fed
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-18 16:32:44 +0700

    Added InsuranceRate schemas

---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+
+
+class InsuranceRateCreate(BaseModel):
+    cargo_type: str
+    rate: float
+    effective_date: date
+
+
+class InsuranceRateResponse(BaseModel):
+    cargo_type: str
+    rate: float
+    effective_date: date
+
+    class Config:
+        orm_mode = True
